5c1fd56f2d9950785060c178c1f0f6f03f732dda
CorentinCrz <33249071+CorentinCrz@users.noreply.github.com>
2019-07-18 16:48:22 +0200

    fix 404

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -6,7 +6,6 @@ use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -21,7 +20,7 @@ class PlayerController extends AbstractController
     {
         $player = $playerRepository->findOneBy(['lastname' => $lastname]);
         if (is_null($player)){
-            throw new NotFoundHttpException();
+            return new JsonResponse(['error'=>'Not found'], Response::HTTP_NOT_FOUND);
         }
         $encoders = [new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
